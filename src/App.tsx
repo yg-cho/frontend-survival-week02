ea79fe52173e342fd6d2fea03b3b9628ad0b4a42
yg-cho <sorisori01@naver.com>
2023-04-18 22:43:05 +0900

    과제 제출

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createElement, useState } from 'react';
 
 import Greeting from './components/Greeting';
@@ -10,8 +9,13 @@ type ImageProps = {
 }
 
 function Image({ src, alt = '', width }: ImageProps) {
-  return (
-    <img src={src} alt={alt} width={width ?? 'auto'} />
+  return createElement(
+    'img',
+    {
+      src,
+      alt,
+      width,
+    },
   );
 }
 
@@ -22,21 +26,12 @@ export default function App() {
     setCount(count + value);
   };
 
-  return (
-    <div>
-      <Greeting name="wholeman" />
-      <Image src="/images/test.jpg" alt="Test Image" width={200} />
-      <p>
-        Count:
-        {' '}
-        {count}
-      </p>
-      {[1, 2, 3, 4, 5].map((i) => React.createElement("button", {
-          key: i,
-          type: "button",
-          onClick: () => handleClick(i)
-        }, "+", i))
-      }
-    </div>
+  return createElement(
+    'div',
+    null,
+    Greeting({ name: 'wholeman' }),
+    createElement(Image, { src: '/images/test.jsp', alt: 'Test Image', width: 200 }),
+    createElement('p', null, 'Count: ', count),
+    [1, 2, 3, 4, 5].map((i) => createElement('button', { type: 'button', key: i, onClick: () => handleClick(i) }, '+', i)),
   );
 }
--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -3,10 +3,11 @@ import { createElement } from 'react';
 export default function Greeting({ name }: {
   name: string;
 }) {
-  return createElement(
-    'p',
-    'Hello',
-    createElement('', null, name),
-    '!',
+  return (
+    createElement(
+      'p',
+      null,
+      `Hello, ${name}!`,
+    )
   );
 }
